47e06cb022953d88cb26c944c6c1437a9b1f8d21
Risheet Lenka <risheetlenka01@gmail.com>
2025-02-18 21:07:33 -0800

    created Model class

--- main.py
+++ main.py
@@ -5,3 +5,16 @@ import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
 
+class Model(nn.Module):
+    def __init__(self, in_features=8, h1=12, h2=14, out_features=2):
+        super().__init__()
+        self.fc1 = nn.Linear(in_features, h1)
+        self.fc2 = nn.Linear(h1, h2)
+        self.out = nn.Linear(h2, out_features)
+
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.out(x)
+
+        return x
